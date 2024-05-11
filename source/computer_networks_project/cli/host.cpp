a5191d6036f03438b9c9dcf76170dbe3ba35527f
fclarkdel <fernandoclarkdel@gmail.com>
2024-05-10 21:44:28 -0500

    clis complete

---
+++ source/computer_networks_project/cli/host.cpp
@@ -0,0 +1,41 @@
+#include <thread>
+
+#include <host/host.hpp>
+
+using namespace computer_networks_project;
+
+int main(int argc, char *argv[]) {
+	std::size_t network_id = std::stoull(argv[1]);
+	std::size_t host_id = std::stoull(argv[2]);
+	std::size_t ethernet_id = std::stoull(argv[3]);
+	std::size_t router_network_id = std::stoull(argv[4]);
+	std::size_t router_host_id = std::stoull(argv[5]);
+	std::size_t bridge_id = std::stoull(argv[6]);
+	std::size_t port_id = std::stoull(argv[7]);
+	std::size_t destination_network_id = std::stoull(argv[8]);
+	std::size_t destination_host_id = std::stoull(argv[9]);
+	std::string data{argv[10]};
+
+	for (std::size_t i = 11; i < argc; ++i)
+		data.append(" ").append(argv[i]);
+
+	host::host host{
+		network_id,
+		host_id,
+		ethernet_id,
+		router_network_id,
+		router_host_id,
+		bridge_id,
+		port_id
+	};
+	host.enqueue_data(
+		destination_network_id,
+		destination_host_id,
+		data
+	);
+	while (true) {
+		host.process_packets();
+
+		std::this_thread::sleep_for(std::chrono::seconds{1});
+	}
+}
--- source/computer_networks_project/cli/router.cpp
+++ source/computer_networks_project/cli/router.cpp
@@ -5,17 +5,31 @@
 using namespace computer_networks_project;
 
 int main(int argc, char *argv[]) {
-	std::size_t id = std::stoull(argv[1]);
-	std::size_t number_of_ports = std::stoull(argv[2]);
-	std::vector<std::size_t> adjacent_bridge_ids{};
+	std::vector<std::size_t> network_ids{};
+	std::vector<std::size_t> host_ids{};
+	std::vector<std::size_t> bridge_ids{};
+	std::vector<std::size_t> port_ids{};
+	std::vector<std::size_t> ethernet_ids{};
 
-	for (std::size_t i = 3; i < argc; ++i)
-		adjacent_bridge_ids.emplace_back(std::stoull(argv[i]));
+	for (std::size_t i = 1; i < argc; i += 5) {
+		network_ids.push_back(std::stoull(argv[i]));
+		host_ids.push_back(std::stoull(argv[i + 1]));
+		bridge_ids.push_back(std::stoull(argv[i + 2]));
+		port_ids.push_back(std::stoull(argv[i + 3]));
+		ethernet_ids.push_back(std::stoull(argv[i + 4]));
+	}
+	router::router router{
+		network_ids,
+		host_ids,
+		bridge_ids,
+		port_ids,
+		ethernet_ids
+	};
+	for (std::size_t i = 0;; ++i) {
+		if (i % 5 == 0)
+			router.hl_broadcast();
 
-	bridge::bridge bridge{id, number_of_ports, adjacent_bridge_ids};
-
-	while (true) {
-		bridge.process_packets();
+		router.process_packets();
 
 		std::this_thread::sleep_for(std::chrono::seconds{1});
 	}
